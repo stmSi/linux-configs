ebc944ba276034beec65ad774846f8dadc6eaad7
stmSi <32661049+stmSi@users.noreply.github.com>
2024-03-22 14:36:29 +0630

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -11,6 +11,19 @@ config.keys = {
   -- Move to the previous tab
   {key="LeftArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1}}
   -- Move to the next tab
-, {key="RightArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=1}}
+, {key="RightArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=1}},
+  -- Custom key bindings for Alt+1 through Alt+8 to switch to tabs 1-8
+  {key="1", mods="CTRL", action=wezterm.action{ActivateTab=0}},
+  {key="2", mods="CTRL", action=wezterm.action{ActivateTab=1}},
+  {key="3", mods="CTRL", action=wezterm.action{ActivateTab=2}},
+  {key="4", mods="CTRL", action=wezterm.action{ActivateTab=3}},
+  {key="5", mods="CTRL", action=wezterm.action{ActivateTab=4}},
+  {key="6", mods="CTRL", action=wezterm.action{ActivateTab=5}},
+  {key="7", mods="CTRL", action=wezterm.action{ActivateTab=6}},
+  {key="8", mods="CTRL", action=wezterm.action{ActivateTab=7}},
+  -- This binds "Alt+v" to split the screen vertically
+  {key="v", mods="ALT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+  -- This binds "Alt+h" to split the screen horizontally
+  {key="h", mods="ALT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
 }
 return config
